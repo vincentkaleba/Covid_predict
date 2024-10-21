559c1e300273c2e5cd558603293da6fd8ad4aa6d
Hyosh code <153867266+vincentkaleba@users.noreply.github.com>
2024-10-21 09:49:01 +0000

    new test

--- data/generate_data.py
+++ data/generate_data.py
@@ -4,7 +4,7 @@ def load_data(filepath):
     """
     Cette function charge les donnée depuis un fichier csv  
     param: filepath - chemain vers le fichier csv (covid_data.csv) 
-    return dataFrame contenantles données
+    return dataFrame contenantles données 
     """
     data = pd.read_csv(filepath)
     return data
